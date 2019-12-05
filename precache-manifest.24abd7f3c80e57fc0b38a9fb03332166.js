5097d6a275a1fb43622a582c702ff0558c16dc29
Axel <axosar@gmail.com>
2019-12-05 16:31:16 -0600

    Updates

---
+++ precache-manifest.24abd7f3c80e57fc0b38a9fb03332166.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2175e4bbee10e0d3d1f7",
+    "url": "/context/static/js/runtime~main.9d122379.js"
+  },
+  {
+    "revision": "4f70fd8c6f4899098ed3",
+    "url": "/context/static/js/main.76851b5c.chunk.js"
+  },
+  {
+    "revision": "f14803cc2a10d36925a3",
+    "url": "/context/static/js/2.6202391c.chunk.js"
+  },
+  {
+    "revision": "4f70fd8c6f4899098ed3",
+    "url": "/context/static/css/main.1a112fc8.chunk.css"
+  },
+  {
+    "revision": "fe8d5af2b6852a5c60124e0a48a19c77",
+    "url": "/context/index.html"
+  }
+];
