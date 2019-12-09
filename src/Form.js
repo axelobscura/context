84523cb7f6017c1af6733b7debeb473541832622
Axel <axosar@gmail.com>
2019-12-09 10:50:21 -0600

    Adding useContext with hooks

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
@@ -34,14 +34,12 @@ const words = {
     remember: "Remember Me"
   }
 }
-class Form extends Component {
-  static contextType = LanguageContext;
-  render() {
-    const { language, changeLanguage } = this.context;
-    const { classes } = this.props;
-    const { signIn, email, password, remember } = words[language];
-    return (
-      <main className={classes.main}>
+function Form(props){
+  const { language, changeLanguage } = useContext(LanguageContext);
+  const { classes } = props;
+  const { signIn, email, password, remember } = words[language];
+  return(
+    <main className={classes.main}>
         <Paper className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
@@ -77,7 +75,15 @@ class Form extends Component {
           </form>
         </Paper>
       </main>
+  )
+}
+/* class Form extends Component {
+  static contextType = LanguageContext;
+  render() {
+    
+    return (
+      
     );
   }
-}
+} */
 export default withStyles(styles)(Form);
